91bc6d386c7530fd9afd21be3d055c1d1fd4b2a3
Nithya-95 <146036380+Nithya-95@users.noreply.github.com>
2024-04-25 00:07:52 +0100

    Create variable.tf

---
+++ multiregion/variable.tf
@@ -0,0 +1,28 @@
+variable "access_key" {
+  description = "value"
+}
+variable "secret_key" {
+  description = "value"
+}
+variable "instance_name" {
+    type = string
+    description = "value"
+    }
+variable "instance_type" {
+    type = string
+    description = "value"
+    }
+variable "ami" {
+    type = string  
+    description = "value"
+}
+variable "ami_id" {
+    type = string  
+    description = "value"
+}
+variable "subnet_id" {
+    description = "value" 
+}
+variable "subnet_id1" {
+    description = "value" 
+}
